f37d2d1a122470cc9cf1accffa30c727dc8e7b71
francoez <francoez@fktech.net>
2022-12-22 16:38:02 -0300

    Change logo and search when press enter

--- src/app/subcomponents/Navbar.tsx
+++ src/app/subcomponents/Navbar.tsx
@@ -1,25 +1,25 @@
 import { Box, Button, Icon, Image, Input, Stack } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import { AiOutlineSearch } from "react-icons/ai";
-
-import Logo from "../../logo.png";
 
 const Navbar: React.FC = () => {
 	const [search, setSearch] = useState("");
 	const [submitted, setSubmitted] = useState(false);
+
+	const history = useHistory();
 
 	function resetSearch() {
 		setSearch("");
 		setSubmitted(false);
 	}
 
-	const SearchIcon = () => (
+	const SearchIcon: React.FC = () => (
 		<Icon as={AiOutlineSearch} color="gray.400" height={6} width={6} />
 	);
 
-	const SearchButton = ({ search }: any) => (
+	const SearchButton: React.FC<{ search: string }> = ({ search }) => (
 		<Button onClick={() => setSubmitted(true)}>
 			{search ? (
 				<Link to={`/items?search=${search}`}>
@@ -33,12 +33,15 @@ const Navbar: React.FC = () => {
 
 	return (
 		<Stack width="100%">
-			<Box paddingY={4} bg="#FEE600" boxShadow="sm" paddingInlineStart={24}>
+			<Box paddingY={2} bg="#FEE600" boxShadow="sm" paddingInlineStart={24}>
 				<Stack direction={{ base: "column", md: "row" }} justifyContent="space-between">
-					<Stack direction="row" flex={1} spacing={12}>
-						<Stack paddingBlockStart={4}>
+					<Stack direction="row" flex={1} spacing={8} align="center">
+						<Stack paddingBlockStart={1}>
 							<Link to={"/"} onClick={resetSearch}>
-								<Image objectFit="contain" src={Logo} />
+								<Image
+									objectFit="contain"
+									src="https://http2.mlstatic.com/frontend-assets/ml-web-navigation/ui-navigation/5.21.17-beta-2/mercadolibre/favicon.svg"
+								/>
 							</Link>
 						</Stack>
 						<Stack
@@ -49,6 +52,7 @@ const Navbar: React.FC = () => {
 							direction="row"
 							maxWidth={600}
 							padding={2}
+							height={12}
 							width="100%"
 						>
 							<Input
@@ -58,6 +62,14 @@ const Navbar: React.FC = () => {
 								fontFamily="inherit"
 								value={search}
 								onChange={(e) => setSearch(e.target.value)}
+								onKeyDown={(e) => {
+									if (e.key === "Enter") {
+										setSubmitted(true);
+										if (search) {
+											history.push(`/items?search=${search}`);
+										}
+									}
+								}}
 								border={submitted && search === "" ? "1px solid red" : ""}
 								style={submitted && search === "" ? { borderColor: "red" } : {}}
 							/>
